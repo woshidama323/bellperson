5db3fd4d74dd1fc822269cfb1d710cdde601b307
Volker Mische <volker.mische@gmail.com>
2021-11-16 17:12:25 +0100

    WIP: remove no longer needed utils

--- src/gpu/mod.rs
+++ src/gpu/mod.rs
@@ -7,9 +7,3 @@ mod locks;
 
 #[cfg(any(feature = "cuda", feature = "opencl"))]
 pub use self::locks::*;
-
-#[cfg(any(feature = "cuda", feature = "opencl"))]
-mod utils;
-
-#[cfg(any(feature = "cuda", feature = "opencl"))]
-pub use self::utils::*;
